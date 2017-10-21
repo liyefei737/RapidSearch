d7886f8066559c31c3bf0304fbed2cfbb88d9ce8
Patrick Hsu <patrick.hsu@mail.utoronto.ca>
2017-10-21 17:37:41 -0400

    google login implemented

--- frontend.py
+++ frontend.py
@@ -1,25 +1,63 @@
-from bottle import get, post, route, run, request
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.client import flow_from_clientsecrets
+from apiclient.discovery import build
+from bottle import get, post, route, run, request, redirect
 from bottle import static_file
+from beaker.middleware import SessionMiddleware
 from heapq import *
 import collections
+import httplib2
 #global data structures
+session_opts = {
+    'session.type': 'file',
+    'session.cookie_expires': 300,
+    'session.data_dir': './data',
+    'session.auto': True
+}
+app = SessionMiddleware(bottle.app(), session_opts)
 
 word_count_dict = {}
 
 min_heap = []
 
+SCOPE = ['https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/userinfo.email']
 
 #[(5, "apple"), ]
 @route('/')
 def search_page():
-	print 'hello1'
-
 	inputString = request.query.keywords
 	if inputString == "":
 		return static_file('frontend.html', root='./static')
 	else:
 		return search_table(inputString)
 
+
+@route('/login')
+def login_trigger():
+	flow = flow_from_clientsecrets('./client_secret.json', scope=SCOPE, redirect_uri="http://localhost:8081/redirect")
+	auth_uri = flow.step1_get_authorize_url()
+
+	redirect(str(auth_uri))
+
+@route('/redirect')
+def redirect_page():
+	code = request.query.get('code','')
+
+	flow = OAuth2WebServerFlow(client_id='619195777450-ea3m50l60rlmbo9ro0abiimmb4o9admp.apps.googleusercontent.com',
+							client_secret='SBurZL_VZPCjaLLVEKGRyD5v',
+							scope=SCOPE,
+							redirect_uri="http://localhost:8081/redirect")
+
+	credentials = flow.step2_exchange(code)
+	token = credentials.id_token['sub']
+	http = httplib2.Http()
+	http = credentials.authorize(http)
+
+	users_service = build('oauth2', 'v2', http=http)
+	user_document = users_service.userinfo().get().execute()
+	print user_document
+	user_email = user_document['email']
+	print user_email
 
 @route('/static/<filename>')
 def server_static(filename):
@@ -87,4 +125,4 @@ def search_table(inputString):
 			
 	return search_result_title + occurence_table + top_twenty
 
-run(host='localhost', port=8081, debug=True)
+run(host='localhost', port=8081, debug=True, app=app)
