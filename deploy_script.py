b02854d00aada2ed5b6e5d6d2dddcb04453481df
liyefei737 <macrofly@gmail.com>
2017-10-21 15:18:18 -0400

    deploy code

---
+++ deploy_script.py
@@ -0,0 +1,17 @@
+#AWS deployment script
+import boto.ec2
+
+***REMOVED*** = "AKIAI76KB5WQP3434LHQ"
+SECRET***REMOVED*** = "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"
+
+ec2 = boto.ec2.connect_to_region("us-east-1", aws_access_key_id=***REMOVED***,aws_secret_access_key=SECRET***REMOVED***)
+keypair_us_east_1 = ec2.create_key_pair("keypair-us-east-1")
+
+#save the keypair's .pem file in the current directory
+keypair_us_east_1.save(".")
+
+security_group = ec2.create_security_group("us-east-1","us-east-1")
+
+# ubuntu image
+new_instances = ec2.run_instances("ami-8caa1ce4") 
+
