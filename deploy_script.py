afb0056716e6584002dbbbe902cdd69e36ad88e3
Yefei <macrofly@gmail.com>
2017-10-22 22:15:43 -0700

    Delete keypair-us-east-1.pem

--- deploy_script.py
+++ deploy_script.py
@@ -1,10 +1,10 @@
 #AWS deployment script
 import boto.ec2
 
-***REMOVED*** = "AKIAI76KB5WQP3434LHQ"
-SECRET***REMOVED*** = "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"
+ACCESSKEY = "AKIAI76KB5WQP3434LHQ"
+SECRETACCESSKEY = "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"
 
-ec2 = boto.ec2.connect_to_region("us-east-1", aws_access_key_id=***REMOVED***,aws_secret_access_key=SECRET***REMOVED***)
+ec2 = boto.ec2.connect_to_region("us-east-1", aws_access_key_id=ACCESSKEY,aws_secret_access_key=SECRETACCESSKEY)
 keypair_us_east_1 = ec2.create_key_pair("keypair-us-east-1")
 
 #save the keypair's .pem file in the current directory
